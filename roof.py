836d67b98087d5f08997046fe221089688284a6a
Fedor <pefbrute@gmail.com>
2023-07-22 00:24:55 +0530

    Refactored functions

--- roof.py
+++ roof.py
@@ -65,12 +65,46 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
 
-async def get_arg(context):
-    if context.args:
-        try:
-            return float(context.args[0])
-        except ValueError:
+def fetch_price(asset: str, fiat: str, trade_type: str, pay_type: str):
+    """
+    Функция для получения цены криптовалюты от Binance P2P.
+    """
+    headers = {
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "content-type": "application/json",
+        "Host": "p2p.binance.com",
+        "Origin": "https://p2p.binance.com",
+        "Pragma": "no-cache",
+        "TE": "Trailers",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
+    }
+
+    data = {
+        "asset": asset,
+        "fiat": fiat,
+        "merchantCheck": True,
+        "page": 1,
+        "payTypes": [pay_type],
+        "publisherType": None,
+        "rows": 3,
+        "tradeType": trade_type
+    }
+
+    try:
+        r = requests.post('https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search', headers=headers, json=data).json()
+
+        if r['data']:
+            return r['data'][2]['adv']['price']
+        else:
+            print(f"No data available for {asset}/{fiat}")
             return None
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
 
 async def fetch_and_format_price(symbol, currency, action, provider):
     try:
@@ -79,80 +113,15 @@ async def fetch_and_format_price(symbol, currency, action, provider):
         print(f"Error fetching price: {e}")
         return None
 
-async def get_rub(update, context):
-    user = update.effective_user
+async def get_arg(context):
+    if context.args:
+        try:
+            return float(context.args[0])
+        except ValueError:
+            return None
 
-    arg = await get_arg(context)
-    if arg is None:
-        await update.message.reply_text("Некорректное значение. Введите число после команды.")
-        return
 
-    USDT_SELL = await fetch_and_format_price("USDT", "LKR", "Sell", "BANK")
-    RUB_BUY = await fetch_and_format_price("USDT", "RUB", "BUY", "TinkoffNew")
-    RUB_LKR = format_price(float(USDT_SELL) / float(RUB_BUY))
 
-    if any(x is None for x in [USDT_SELL, RUB_BUY, RUB_LKR]):
-        await update.message.reply_text("Error fetching some prices, please try again later.")
-        return
-
-    response = (
-        f"Купить USDT/RUB: {RUB_BUY}\n"
-        f"Продать USDT/LKR: {USDT_SELL}\n\n"
-        f"{format_info('Безубыток RUB', RUB_LKR, [0.40, 0.35, 0.30, 0.25, 0.20, 0.15], arg)}"
-    )
-
-    await update.message.reply_text(response)
-
-async def get_usdt(update, context):
-    user = update.effective_user
-
-    arg = await get_arg(context)
-    if arg is None:
-        await update.message.reply_text("Некорректное значение. Введите число после команды.")
-        return
-
-    USDT_SELL = await fetch_and_format_price("USDT", "LKR", "Sell", "BANK")
-    if USDT_SELL is None:
-        await update.message.reply_text("Error fetching prices, please try again later.")
-        return
-
-    response = f"{format_info('Безубыток USDT', USDT_SELL, [19, 17, 15, 13, 12.5, 11, 10], arg)}\n"
-
-    await update.message.reply_text(response)
-
-async def get_usdt_percentages(update, context):
-    user = update.effective_user
-
-    arg = await get_arg(context)
-    if arg is None:
-        await update.message.reply_text("Некорректное значение. Введите число после команды.")
-        return
-
-    USDT_SELL = await fetch_and_format_price("USDT", "LKR", "Sell", "BANK")
-    if USDT_SELL is None:
-        await update.message.reply_text("Error fetching prices, please try again later.")
-        return
-
-    response = f"{format_info_percentage('Безубыток USDT', USDT_SELL, [3, 2.5, 2, 1.5, 1], arg)}\n\n"
-
-    await update.message.reply_text(response)
-
-async def get_usdt_add_percentages(update, context):
-    user = update.effective_user
-
-    arg = await get_arg(context)
-    if arg is None:
-        await update.message.reply_text("Некорректное значение. Введите число после команды.")
-        return
-
-    USDT_SELL = await fetch_and_format_price("USDT", "LKR", "Sell", "BANK")
-    if USDT_SELL is None:
-        await update.message.reply_text("Error fetching prices, please try again later.")
-        return
-
-    response = f"{format_info_add_percentage('Безубыток USDT', USDT_SELL, [1, 1.5, 2, 2.5, 3], arg)}\n"
-
-    await update.message.reply_text(response)
 
 
 
@@ -198,24 +167,7 @@ async def generate_responses(arg, symbol, price, ranges, ranges_colombo, locatio
     ]
     return responses if arg in [symbol.lower(), None] else []
 
-async def print_prices(update, context):
-    user = update.effective_user
 
-    arg = context.args[0].lower() if context.args else None
-
-    USDT_SELL = await get_price("USDT", "LKR", "Sell", "BANK")
-    RUB_LKR = await get_price("USDT", "RUB", "BUY", "TinkoffNew")
-    if USDT_SELL is None or RUB_LKR is None:
-        await update.message.reply_text("Error fetching prices, please try again later.")
-        return
-
-    responses = await asyncio.gather(
-        generate_responses(arg, 'RUB', RUB_LKR, RUB_RANGES, RUB_RANGES_COLOMBO, 'Хиккадува - Матара', 'Коломбо, Бентота'),
-        generate_responses(arg, 'USDT', USDT_SELL, USDT_RANGES, USDT_RANGES_COLOMBO, 'Хиккадува - Матара', 'Коломбо, Бентота')
-    )
-
-    full_response = "\n".join(sum(responses, []))
-    await update.message.reply_text(full_response)
 
 def generate_price_response(currency, ranges, base_price, location):
     response = f"{location}:\n"    
@@ -255,6 +207,41 @@ async def get_rates():
     RUB_LKR = float(format_price(float(USDT_SELL) / float(RUB_BUY)))
 
     return RUB_LKR
+
+
+
+def format_profit(profit):
+    return "{:,.0f}".format(float(profit)).replace(",", " ")    
+
+
+
+def format_price(price):
+    return "{:,.2f}".format(float(price)).replace(",", " ")
+
+def format_info(prefix, price, deductions, multiplier=None):
+    info = f"{prefix}: {price}\n\n"
+    for deduction in deductions:
+        new_price = format_price((float(price) - deduction))
+        if multiplier:
+            profit = format_profit(deduction * multiplier)
+            info += f"Курс: {new_price}\n - Разница: {deduction}\n - Профит: {profit} Рупий\n\n"
+        else:
+            info += f"Курс: {new_price}\n - Разница: {deduction}\n\n"
+    info += "\n"
+    return info
+    
+def format_info_percentage(prefix, price, percentages, multiplier=None):
+    info = f"{prefix}: {price}\n\n"
+    for percentage in percentages:
+        new_price = format_price(float(price) * (1 - percentage / 100.0))
+        if multiplier:
+            profit = format_profit((float(price) - float(new_price)) * multiplier)
+            info += f"Процент: {percentage}%\n- Курс: {new_price}\n- Профит: {profit} Рупий\n\n"
+        else:
+            info += f"Процент: {percentage}%\n- Курс: {new_price}\n"
+    info += "\n"
+    return info
+
 
 
 
@@ -359,148 +346,135 @@ async def generate_response_lkr_usdt(summa, custom_rate, location, USDT_SELL):
 
     return response, response2, response3, response4
 
+async def process_exchange(update, context, currency, response_generator, get_rates_func=None):
+    user = update.effective_user
+    summa, custom_rate, location = await get_user_arguments(context)
 
+    if get_rates_func:
+        rate = await get_rates_func()
+    else:
+        rate = float(format_price(fetch_price(currency, "LKR", "Sell", "BANK")))
+
+    response = f"Безубыток: {format_price(rate)}\n"
+    await update.message.reply_text(response)
+
+    responses = await response_generator(summa, custom_rate, location, rate)
+
+    for response in responses:
+        await update.message.reply_text(response)
+
+
+
+
+async def get_rub(update, context):
+    user = update.effective_user
+
+    arg = await get_arg(context)
+    if arg is None:
+        await update.message.reply_text("Некорректное значение. Введите число после команды.")
+        return
+
+    USDT_SELL = await fetch_and_format_price("USDT", "LKR", "Sell", "BANK")
+    RUB_BUY = await fetch_and_format_price("USDT", "RUB", "BUY", "TinkoffNew")
+    RUB_LKR = format_price(float(USDT_SELL) / float(RUB_BUY))
+
+    if any(x is None for x in [USDT_SELL, RUB_BUY, RUB_LKR]):
+        await update.message.reply_text("Error fetching some prices, please try again later.")
+        return
+
+    response = (
+        f"Купить USDT/RUB: {RUB_BUY}\n"
+        f"Продать USDT/LKR: {USDT_SELL}\n\n"
+        f"{format_info('Безубыток RUB', RUB_LKR, [0.40, 0.35, 0.30, 0.25, 0.20, 0.15], arg)}"
+    )
+
+    await update.message.reply_text(response)
+
+async def get_usdt(update, context):
+    user = update.effective_user
+
+    arg = await get_arg(context)
+    if arg is None:
+        await update.message.reply_text("Некорректное значение. Введите число после команды.")
+        return
+
+    USDT_SELL = await fetch_and_format_price("USDT", "LKR", "Sell", "BANK")
+    if USDT_SELL is None:
+        await update.message.reply_text("Error fetching prices, please try again later.")
+        return
+
+    response = f"{format_info('Безубыток USDT', USDT_SELL, [19, 17, 15, 13, 12.5, 11, 10], arg)}\n"
+
+    await update.message.reply_text(response)
+
+async def get_usdt_percentages(update, context):
+    user = update.effective_user
+
+    arg = await get_arg(context)
+    if arg is None:
+        await update.message.reply_text("Некорректное значение. Введите число после команды.")
+        return
+
+    USDT_SELL = await fetch_and_format_price("USDT", "LKR", "Sell", "BANK")
+    if USDT_SELL is None:
+        await update.message.reply_text("Error fetching prices, please try again later.")
+        return
+
+    response = f"{format_info_percentage('Безубыток USDT', USDT_SELL, [3, 2.5, 2, 1.5, 1], arg)}\n\n"
+
+    await update.message.reply_text(response)
+
+async def get_usdt_add_percentages(update, context):
+    user = update.effective_user
+
+    arg = await get_arg(context)
+    if arg is None:
+        await update.message.reply_text("Некорректное значение. Введите число после команды.")
+        return
+
+    USDT_SELL = await fetch_and_format_price("USDT", "LKR", "Sell", "BANK")
+    if USDT_SELL is None:
+        await update.message.reply_text("Error fetching prices, please try again later.")
+        return
+
+    response = f"{format_info_add_percentage('Безубыток USDT', USDT_SELL, [1, 1.5, 2, 2.5, 3], arg)}\n"
+
+    await update.message.reply_text(response)
+
+async def print_prices(update, context):
+    user = update.effective_user
+
+    arg = context.args[0].lower() if context.args else None
+
+    USDT_SELL = await get_price("USDT", "LKR", "Sell", "BANK")
+    RUB_LKR = await get_price("USDT", "RUB", "BUY", "TinkoffNew")
+    if USDT_SELL is None or RUB_LKR is None:
+        await update.message.reply_text("Error fetching prices, please try again later.")
+        return
+
+    responses = await asyncio.gather(
+        generate_responses(arg, 'RUB', RUB_LKR, RUB_RANGES, RUB_RANGES_COLOMBO, 'Хиккадува - Матара', 'Коломбо, Бентота'),
+        generate_responses(arg, 'USDT', USDT_SELL, USDT_RANGES, USDT_RANGES_COLOMBO, 'Хиккадува - Матара', 'Коломбо, Бентота')
+    )
+
+    full_response = "\n".join(sum(responses, []))
+    await update.message.reply_text(full_response)
 
 async def get_rub_lkr(update, context):
-    user = update.effective_user
-
-    summa, custom_rate, location = await get_user_arguments(context)
-
-    # Запрашиваем курсы валют
-    RUB_LKR = await get_rates()
-
-    response = f"Безубыток: {format_price(RUB_LKR)}\n"
-    await update.message.reply_text(response)
-
-    response, response2, response3, response4 = await generate_response(summa, custom_rate, location, RUB_LKR)
-
-    await update.message.reply_text(response)
-    await update.message.reply_text(response2)
-    await update.message.reply_text(response3)
-    await update.message.reply_text(response4)
+    await process_exchange(update, context, "RUB", generate_response, get_rates)
 
 async def get_lkr_rub(update, context):
-    user = update.effective_user
-
-    summa, custom_rate, location = await get_user_arguments(context)
-
-    RUB_LKR = await get_rates()
-
-    response = f"Безубыток: {format_price(RUB_LKR)}\n"
-    await update.message.reply_text(response)
-
-    response, response2, response3, response4 = await generate_response_lkr_rub(summa, custom_rate, location, RUB_LKR)
-
-    await update.message.reply_text(response)
-    await update.message.reply_text(response2)
-    await update.message.reply_text(response3)
-    await update.message.reply_text(response4)
+    await process_exchange(update, context, "RUB", generate_response_lkr_rub, get_rates)
 
 async def get_usdt_lkr(update, context):
-    user = update.effective_user
-
-    summa, custom_rate, location = await get_user_arguments(context)
-
-    USDT_SELL = float(format_price(fetch_price("USDT", "LKR", "Sell", "BANK")))
-
-    response = f"Безубыток: {format_price(USDT_SELL)}\n"
-    await update.message.reply_text(response)
-
-    response, response2, response3, response4 = await generate_response_usdt_lkr(summa, custom_rate, location, USDT_SELL)
-
-    await update.message.reply_text(response)
-    await update.message.reply_text(response2)
-    await update.message.reply_text(response3)
-    await update.message.reply_text(response4)
+    await process_exchange(update, context, "USDT", generate_response_usdt_lkr)
 
 async def get_lkr_usdt(update, context):
-    user = update.effective_user
-
-    summa, custom_rate, location = await get_user_arguments(context)
-
-    USDT_SELL = float(format_price(fetch_price("USDT", "LKR", "Sell", "BANK")))
-
-    response = f"Безубыток: {format_price(USDT_SELL)}\n"
-    await update.message.reply_text(response)
-
-    response, response2, response3, response4 = await generate_response_lkr_usdt(summa, custom_rate, location, USDT_SELL)
-
-    await update.message.reply_text(response)
-    await update.message.reply_text(response2)
-    await update.message.reply_text(response3)
-    await update.message.reply_text(response4)
+    await process_exchange(update, context, "USDT", generate_response_lkr_usdt)
 
 
-def format_profit(profit):
-    return "{:,.0f}".format(float(profit)).replace(",", " ")    
 
-def fetch_price(asset: str, fiat: str, trade_type: str, pay_type: str):
-    """
-    Функция для получения цены криптовалюты от Binance P2P.
-    """
-    headers = {
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
-        "Cache-Control": "no-cache",
-        "Connection": "keep-alive",
-        "content-type": "application/json",
-        "Host": "p2p.binance.com",
-        "Origin": "https://p2p.binance.com",
-        "Pragma": "no-cache",
-        "TE": "Trailers",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0"
-    }
 
-    data = {
-        "asset": asset,
-        "fiat": fiat,
-        "merchantCheck": True,
-        "page": 1,
-        "payTypes": [pay_type],
-        "publisherType": None,
-        "rows": 3,
-        "tradeType": trade_type
-    }
-
-    try:
-        r = requests.post('https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search', headers=headers, json=data).json()
-
-        if r['data']:
-            return r['data'][2]['adv']['price']
-        else:
-            print(f"No data available for {asset}/{fiat}")
-            return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
-
-def format_price(price):
-    return "{:,.2f}".format(float(price)).replace(",", " ")
-
-def format_info(prefix, price, deductions, multiplier=None):
-    info = f"{prefix}: {price}\n\n"
-    for deduction in deductions:
-        new_price = format_price((float(price) - deduction))
-        if multiplier:
-            profit = format_profit(deduction * multiplier)
-            info += f"Курс: {new_price}\n - Разница: {deduction}\n - Профит: {profit} Рупий\n\n"
-        else:
-            info += f"Курс: {new_price}\n - Разница: {deduction}\n\n"
-    info += "\n"
-    return info
-    
-def format_info_percentage(prefix, price, percentages, multiplier=None):
-    info = f"{prefix}: {price}\n\n"
-    for percentage in percentages:
-        new_price = format_price(float(price) * (1 - percentage / 100.0))
-        if multiplier:
-            profit = format_profit((float(price) - float(new_price)) * multiplier)
-            info += f"Процент: {percentage}%\n- Курс: {new_price}\n- Профит: {profit} Рупий\n\n"
-        else:
-            info += f"Процент: {percentage}%\n- Курс: {new_price}\n"
-    info += "\n"
-    return info
 
 def main() -> None:
     """Start the bot."""
